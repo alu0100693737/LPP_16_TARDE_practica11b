e6e17db5a7308557b6898b420f230cdf9c145158
Ivan Garcia <alu0100693737@ull.edu.es>
2014-12-09 19:18:43 +0100

    clase Quiz y estructura hecha

--- spec/prct10_spec.rb
+++ spec/prct10_spec.rb
@@ -10,6 +10,33 @@ describe Prct10 do
 		      it "tiene que tener un enunciado" do
 		      end
 		  end
+
+if __FILE__ == $0
+quiz = Quiz.new("Cuestionario de PFS 10/12/2011") {
+questions 'En que anyo Cristobal Colon descubrio America',
+wrong =>'1942',
+right =>'1492',
+wrong =>'1808',
+wrong =>'1914'
+
+a = rand(10)
+b = rand(10)
+questions   "#{a}+#{b} = ",
+wrong =>"44",
+wrong =>"#{a + b + 2}",
+right =>"#{a + b}",
+wrong =>"#{a + b - 2}"
+}
+
+
+puts quiz
+puts "************************"
+quiz.run
+end
+
+
+
+
 end	
 
   
