8dd6e53b67cd5beea2dcd5bb181f3895469ea7a1
Fabián Díaz Lorenzo <alu0100702545@ull.edu.es>
2014-12-11 15:30:28 +0000

    Practica finalizada

--- lib/prct10/DSL_prct11.rb
+++ lib/prct10/DSL_prct11.rb
@@ -13,7 +13,8 @@ module Prct10
 	n = 1
 	cadena << "  -> #{opciones[:right]}\n"
 	n+=1
-	opciones[:wrong].each do |op|
+	#for int i = i<counter
+	opciones.each_with_index do |op|
 	  cadena << "  -> #{op}\n"
 	  n+=1
 	end
@@ -22,8 +23,8 @@ module Prct10
   end
   
   class Quiz
-    attr_accessor :title, :questions
-    
+    attr_accessor :title, :questions,:counter
+    @@counter=0
     
     def initialize(title = "", &block) #titulo y bloque con question(cadena) y hash
       self.title = title
@@ -53,11 +54,17 @@ module Prct10
     def pregunta(name1, options = {})
       pregunta = Question.new(name1, options)
       questions << pregunta
+      
     #pregunta << " (#{options[:wrong]})" if options[:wrong]
     end
-
-    def wrong (option)
-      self.questions[-1].opciones[:wrong] << option
+    def contador
+      @@counter+=1
+    end
+    def wrong #(option)
+      self.contador
+      
+    
+     "wrong#{@counter} :"
     end
   end
 end
@@ -65,18 +72,5 @@ end
 
 
 
-#   def respuesta(text, options = {})
-#     respuesta= text
-#     respuesta << " (#{options[:during]})" if options[:during]
-# 
-#     answers << respuesta
-#   end
-
-#     def run
-#       counter = 0
-#       puts self.name+ "\n\n"
-#       self.questions.each { |q| counter +=1}
-#       puts "#{counter} respuestas correctas de un total de #{questions.size}."
-#     end
     
 
--- spec/prct10_spec.rb
+++ spec/prct10_spec.rb
@@ -6,26 +6,27 @@ describe Prct10 do
 		  before :each do
 		@quiz = Prct10::Quiz.new("Cuestionario de PFS 10/12/2011"){
 			  pregunta 'En que anyo Cristobal Colon descubrio America', 
-			    :right =>'1492',:wrong => []
-			    wrong '1942'
-			    wrong '1808'
-			    wrong '1914'
+			    :right =>'1492',
+		            wrong => '1942',
+			    wrong => '1808',
+			    wrong => '1914'
+		
 			  a = rand(10)
 			  b = rand(10)
 			  pregunta "#{a}+#{b} = ",
-			    :right =>"#{a + b}",:wrong => []    
-			    wrong '44'
-			    wrong "#{a + b + 2}"
-			    wrong "#{a + b - 2}"
+			    :right =>"#{a + b}",   
+			    wrong =>'44',
+			    wrong =>"#{a + b + 2}",
+			    wrong =>"#{a + b - 2}"
 			  pregunta 'En Ruby los bloque son objetos que continen codigo',
-			    :right=>'Falso',:wrong => []
-			    wrong 'Cierto'
+			    :right =>'Falso',
+			    wrong => 'Cierto'
 			  pregunta 'Cuantos argumentos de tipo bloque puede recibir un metodo?',
-			    :right => '1',:wrong => []
-			    wrong '2'
-			    wrong 'muchos'
-			    wrong 'los que defina el usuario'
-			 
+			    :right => '1',
+			    wrong => '2',
+			    wrong => 'muchos',
+			    wrong =>'los que defina el usuario'
+# 			 
 		  }
 		  end
 		  context "La clase Quiz contiene los siguientes metodos" do
@@ -33,7 +34,7 @@ describe Prct10 do
 			expect(@quiz).to respond_to :to_s
 		      end
 		      it "metodo pregunta" do
-			expect(@quiz.to_s).to match('En Ruby los bloque son objetos que continen codigo')
+			expect(@quiz.to_s).to match('Cuantos argumentos de tipo bloque puede recibir un metodo')
 		      end
 		  end
   end
